9f672cda91e9b63261df34c2d1c0367cf0512368
Rokusuke69 <chauhan.abhishek.motilal@gmail.com>
2025-10-29 19:28:04 +0530

    Initial commit

---
+++ src/db/db.js
@@ -0,0 +1,11 @@
+const mongoose  = require('mongoose');
+
+
+function connectDB() {
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+         console.log('MongoDB connected successfully');        
+    })
+}
+
+module.exports = connectDB
